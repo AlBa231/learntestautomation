99a5240b31a022caa98ab970fa1b8bc9266639d4
AlBa231 <alba231@gmail.com>
2022-02-15 11:02:29 +0200

    feat(lesson1): add app and test for practic lesson 1

---
+++ FindMaxDifferentSymbols/FindMaxDifferentSymbols/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter a string:");
+var str = Console.ReadLine();
+if (string.IsNullOrEmpty(str))
+{
+    Console.WriteLine("Provided string is empty.");
+    return;
+}
+
+var differentSymbolCount = new FindMaxDifferentSymbols.SymbolDetector(str).DetectDifferentSymbols();
+Console.WriteLine($"Maximum number of unequal consecutive characters in a string: {differentSymbolCount}");
