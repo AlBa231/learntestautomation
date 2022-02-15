5caeeb069ffbc0ef70c379a6bd8dce617fc7fec3
AlBa231 <alba231@gmail.com>
2022-02-15 21:00:35 +0200

    feat(lesson2): add convert to radix app and tests

---
+++ ConvertNumberToXNotation/NumberRadixConverter.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+namespace ConvertNumberToXNotation
+{
+    public class NumberRadixConverter
+    {
+        private const int MinRadix = 2;
+        private const int MaxRadix = 20;
+
+        private static readonly char[] RadixSymbols = "0123456789ABCDEFGHIJ".ToCharArray();
+
+        private readonly int radix;
+
+        public NumberRadixConverter(int radix)
+        {
+            ValidateRadix(radix);
+            this.radix = radix;
+        }
+
+        private void ValidateRadix(int radix)
+        {
+            if (radix < MinRadix || radix > MaxRadix)
+                throw new NumberRadixException($"The radix must be in range from {MinRadix} to {MaxRadix}.");
+        }
+
+        public string Convert(int number)
+        {
+            ValidateNumber(number);
+            StringBuilder result = new StringBuilder();
+
+            int residue = number;
+
+            while (residue >= radix)
+            {
+                result.Insert(0, GetSymbol(residue % radix));
+                residue /= radix;
+            }
+            result.Insert(0, GetSymbol(residue));
+
+            return result.ToString();
+        }
+
+        private static void ValidateNumber(int number)
+        {
+            if (number <= 0)
+                throw new NumberRadixException("Number must be more that zero.");
+        }
+
+        private char GetSymbol(int residueFromRadixDivision)
+        {
+            return RadixSymbols[residueFromRadixDivision];
+        }
+    }
+}
---
+++ ConvertNumberToXNotation/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConvertNumberToXNotation;
+
+var number = ReadNumberWithMessage("Enter a number to convert:");
+var radix = ReadNumberWithMessage("Enter a Radix to convert number to:");
+
+var convertedNumber = new NumberRadixConverter(radix).Convert(number);
+Console.WriteLine($" {number} in {radix} positional notation is - " + convertedNumber);
+
+int ReadNumberWithMessage(string message)
+{
+    Console.WriteLine(message);
+    var strNumber = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(strNumber))
+        throw new NumberRadixException($"{strNumber} is not valid number");
+    return int.Parse(strNumber);
+}
