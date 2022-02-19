39dd040e90f91262f3b86a466170f8f28d7a3662
AlBa231 <alba231@gmail.com>
2022-02-19 08:12:03 +0200

    feat(module1): find unequal consecutive characters project and test

---
+++ FindMaxDifferentSymbols/SymbolDetector.cs
@@ -0,0 +1,31 @@
+﻿namespace FindMaxDifferentSymbols
+{
+    public class SymbolDetector
+    {
+        private readonly string line;
+
+        public SymbolDetector(string line)
+        {
+            this.line = line ?? throw new ArgumentNullException(nameof(line));
+        }
+
+        public int DetectDifferentSymbols()
+        {
+            char lastCheckedChar = (char)0;
+            int differentSymbolCount = 0;
+            int maxDifferentSymbolCount = 0;
+            foreach(char checkSymbol in line)
+            {
+                if (checkSymbol == lastCheckedChar)
+                {
+                    maxDifferentSymbolCount = Math.Max(differentSymbolCount, maxDifferentSymbolCount);
+                    differentSymbolCount = 0;
+                }
+                differentSymbolCount++;
+                lastCheckedChar = checkSymbol;
+            }
+            maxDifferentSymbolCount = Math.Max(differentSymbolCount, maxDifferentSymbolCount);
+            return maxDifferentSymbolCount;
+        }
+    }
+}
