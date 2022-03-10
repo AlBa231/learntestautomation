89da53f0e14d871bab51d34d40fbcaf28805c1e5
AlBa231 <Oleksandr_Barabanov@epam.com>
2022-03-10 21:00:30 +0200

    feat(vehiclefactory): add CreateCar

---
+++ VehiclePrinter.Test/VehicleGeneratorTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehiclePrinter.Models;
+using Xunit;
+
+namespace VehiclePrinter.Test
+{
+    public class VehicleGeneratorTests
+    {
+        [Fact]
+        public void TestVehicleFactory_CreateCar()
+        {
+            var car = VehicleFactory.CreateCar();
+            
+            Assert.Equal(@"Toyota Camry 2.4 (170 hp), Gasoline (2AZ-FE).
+4 wheels, 570 kg, 231000В0102526,
+Automatic, 5 gears, Aisin Japan", car.ToString());
+        }
+    }
+}
---
+++ VehiclePrinter/VehicleFactory.cs
@@ -0,0 +1,41 @@
+﻿using VehiclePrinter.Models;
+
+namespace VehiclePrinter
+{
+    public static class VehicleFactory
+    {
+        public static Vehicle CreateCar()
+        {
+            var engine = new VehicleEngine
+            {
+                Power = 170,
+                Capacity = 2362,
+                Type = EngineType.Gasoline,
+                SerialNumber = "2AZ-FE"
+            };
+
+            var chassis = new VehicleChassis
+            {
+                WheelCount = 4,
+                Number = "231000В0102526",
+                MaxLoad = 570
+            };
+
+            var transmission = new Transmission
+            {
+                Type = TransmissionType.Automatic,
+                GearsNumber = 5,
+                Manufacturer = "Aisin Japan"
+            };
+
+            return new Car
+            {
+                Engine = engine,
+                Chassis = chassis,
+                Transmission = transmission,
+                Model = "Camry",
+                Make = "Toyota"
+            };
+        }
+    }
+}
