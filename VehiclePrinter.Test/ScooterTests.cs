963d4d7d9294ebf5b23ca2e6809654066b8f0f33
AlBa231 <Oleksandr_Barabanov@epam.com>
2022-03-10 16:42:31 +0200

    feat(scooter): add Scooter model

---
+++ VehiclePrinter.Test/ScooterTests.cs
@@ -0,0 +1,50 @@
+using VehiclePrinter.Models;
+using Xunit;
+
+namespace VehiclePrinter.Test;
+
+public class ScooterTests
+{
+    [Fact]
+    public void TestScooterToString()
+    {
+        var engine = new VehicleEngine
+        {
+            Power = 8,
+            Capacity = 125,
+            Type = EngineType.Gasoline,
+            SerialNumber = "2ABS22Z-F3123AS11"
+        };
+
+        var chassis = new VehicleChassis
+        {
+            WheelCount = 2,
+            Number = "1235324402321E",
+            MaxLoad = 180
+        };
+
+        var transmission = new Transmission
+        {
+            Type = TransmissionType.Manual,
+            GearsNumber = 2,
+            Manufacturer = "CHOHO 428"
+        };
+
+        var scooter = new Scooter
+        {
+            Engine = engine,
+            Chassis = chassis,
+            Transmission = transmission,
+            Model = "Alfa",
+            Make = "MUSSTANG",
+            HasAlarm = true,
+            SeatCount = 1
+        };
+
+
+        Assert.Equal(@"MUSSTANG Alfa 0.1 (8 hp), Gasoline (2ABS22Z-F3123AS11).
+2 wheels, 180 kg, 1235324402321E,
+Manual, 2 gears, CHOHO 428
+Seats: 1, Alarm", scooter.ToString());
+    }
+}
---
+++ VehiclePrinter/Models/Scooter.cs
@@ -0,0 +1,17 @@
+﻿namespace VehiclePrinter.Models;
+
+public class Scooter : Vehicle
+{
+    public int SeatCount { get; init; }
+
+    public bool HasAlarm { get; init; }
+    public string HasAlarmPrefix => HasAlarm ? ", Alarm" : string.Empty;
+
+    public override string ToString()
+    {
+        return base.ToString() + @$"
+Seats: {SeatCount}{HasAlarmPrefix}";
+    }
+
+
+}
