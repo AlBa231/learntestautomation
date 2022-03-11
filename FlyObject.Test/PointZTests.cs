4f90859345236fd6d519ce702277dc7e5367fdb6
AlBa231 <Oleksandr_Barabanov@epam.com>
2022-03-11 12:28:28 +0200

    feat(point): ability to find diff for two points

---
+++ FlyObject.Test/PointZTests.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+
+namespace FlyObject.Test
+{
+    public class PointZTests
+    {
+        [Fact]
+        public void TestPointZDiff_Equals()
+        {
+            var start = new PointZ(10, 10, 10);
+            var end = new PointZ(20, 20, 20);
+            var diff = end - start;
+            Assert.Equal(10, diff.X);
+            Assert.Equal(10, diff.Y);
+            Assert.Equal(10, diff.Z);
+        }
+
+
+        [Fact]
+        public void TestPointZDiff_SmallNumbers()
+        {
+            var start = new PointZ(10, 10, 10);
+            var end = new PointZ(25, 23, 21);
+            var diff = end - start;
+            Assert.Equal(15, diff.X);
+            Assert.Equal(13, diff.Y);
+            Assert.Equal(11, diff.Z);
+        }
+
+
+        [Fact]
+        public void TestPointZDiff_NegativeNumbers()
+        {
+            var start = new PointZ(10, 10, 10);
+            var end = new PointZ(5, 3, 7);
+            var diff = end - start;
+            Assert.Equal(-5, diff.X);
+            Assert.Equal(-7, diff.Y);
+            Assert.Equal(-3, diff.Z);
+        }
+    }
+}
---
+++ FlyObject/PointZ.cs
@@ -0,0 +1,9 @@
+﻿namespace FlyObject;
+
+public record PointZ(int X, int Y, int Z)
+{
+    public static PointZ operator -(PointZ a, PointZ b)
+    {
+        return new PointZ(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+    }
+}
