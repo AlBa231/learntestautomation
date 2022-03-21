ac130ca821ed818aa057b7c4227202118eeb59c1
AlBa231 <Oleksandr_Barabanov@epam.com>
2022-03-21 15:31:40 +0200

    test(restriction): add check for no error

--- FlyObject.Test/RestrictionTests.cs
+++ FlyObject.Test/RestrictionTests.cs
@@ -1,11 +1,11 @@
-﻿using FlyObject.Lib.Restrictions;
+﻿using System.Linq;
+using FlyObject.Lib.Restrictions;
 using Xunit;
 
 namespace FlyObject.Test
 {
     public class RestrictionTests
     {
-
         [Fact]
         public void TestAddWithReplaceRestriction()
         {
@@ -13,6 +13,19 @@ namespace FlyObject.Test
             flyable.Restrictions.Add(new MinSpeedRestriction(2));
 
             Assert.Throws<FlyableRestrictionException>(() => flyable.GetFlyTime(new PointZ("1 2 3")));
+        }
+
+        [Fact]
+        public void TestNoRestrictionForBird()
+        {
+            var flyable = new Bird { Speed = 10 };
+            flyable.Restrictions.Add(new MinSpeedRestriction(2));
+
+            flyable.GetFlyTime(new PointZ("1 2 3"));
+
+            var minSpeedRestriction = flyable.Restrictions.OfType<MinSpeedRestriction>().First();
+
+            Assert.Equal(2, minSpeedRestriction.MinimalSpeed);
         }
     }
 }
