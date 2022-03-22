a4b164814f52f02438f059a20d740ce11bd0c03c
AlBa231 <Oleksandr_Barabanov@epam.com>
2022-03-22 13:23:20 +0200

    feat(factory): added RandomVehicleFactory

--- VehiclePrinter/Program.cs
+++ VehiclePrinter/Program.cs
@@ -2,22 +2,18 @@
 using VehiclePrinter;
 using VehiclePrinter.Models;
 
-var cars = new Vehicle[]
-{
-    VehicleFactory.CreateCar(),
-    VehicleFactory.CreateTruck(),
-    VehicleFactory.CreateBus(),
-    VehicleFactory.CreateScooter()
-};
+var vehicles = new List<Vehicle>(new int[10].Select(_ => RandomVehicleFactory.CreateRandomVehicle()));
 
 Console.WriteLine("Vehicles with specifications:!");
 
-for (var i = 0; i < cars.Length; i++)
+for (var i = 0; i < vehicles.Count; i++)
 {
-    Console.WriteLine(GetVehicleHeader(cars[i], i));
-    Console.WriteLine(cars[i]);
+    Console.WriteLine(GetVehicleHeader(vehicles[i], i));
+    Console.WriteLine(vehicles[i]);
     Console.WriteLine(new string('=', 80));
     Console.WriteLine();
 }
+
+
 
 string GetVehicleHeader(Vehicle vehicle, int index) => $"{index + 1}. {vehicle.GetType().Name}";
